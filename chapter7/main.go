0bca0584e811714efc0d37fd3c516c98715428be
Christopher Lam <chrislam67@gmail.com>
2015-08-25 21:56:54 -0400

    finished off ch 7 exercises

--- chapter7/main.go
+++ chapter7/main.go
@@ -26,7 +26,32 @@ func main() {
 	fmt.Println(increment())
 	fmt.Println(increment())
 
-	factorial(4)
+	factorialSum := factorial(4)
+	fmt.Println("factorial sum", factorialSum)
+
+	defer second()
+	first()
+
+	//	defer func() {
+	//		str := recover()
+	//		fmt.Println(str)
+	//	}()
+	//	panic("PANIC")
+
+	sumArray := []int{1, 2, 3, 4, 5}
+	sumVal := sum(sumArray)
+	fmt.Println("sum", sumVal)
+
+	fmt.Println(oddEvenHalf(2))
+	fmt.Println(oddEvenHalf(5))
+
+	fmt.Println(maxValueInVariadic(1, 6, 2, 4, 9, 0))
+
+	fmt.Println("fib 0", fib(0))
+	fmt.Println("fib 1", fib(1))
+	fmt.Println("fib 2", fib(2))
+	fmt.Println("fib 3", fib(3))
+	fmt.Println("fib 4", fib(4))
 }
 
 func average(xs []float64) float64 {
@@ -59,4 +84,50 @@ func factorial(x uint) uint {
 	}
 
 	return x * factorial(x-1)
+}
+
+func first() {
+	fmt.Println("first")
+}
+
+func second() {
+	fmt.Println("second")
+}
+
+func sum(x []int) int {
+	total := 0
+	for _, value := range x {
+		total += value
+	}
+	return total
+}
+
+func oddEvenHalf(x int) (int, bool) {
+	var evenOdd bool
+
+	if x%2 == 0 {
+		evenOdd = true
+	}
+
+	return x / 2, evenOdd
+}
+
+func maxValueInVariadic(x ...int) int {
+	max := 0
+
+	for _, val := range x {
+		if val > max {
+			max = val
+		}
+	}
+
+	return max
+}
+
+func fib(x int) int {
+	if x == 0 || x == 1 {
+		return x
+	}
+
+	return fib(x-2) + fib(x-1)
 }
