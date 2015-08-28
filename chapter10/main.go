da4a53d49ed2e215732cf845dd20b12aa2867f9f
Christopher Lam <chrislam67@gmail.com>
2015-08-27 20:28:57 -0400

    added and finished ch 10

---
+++ chapter10/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func f(n int) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(n, ":", i)
+		amt := time.Duration(rand.Intn(250))
+		time.Sleep(time.Millisecond * amt)
+	}
+}
+
+//func pinger(c chan<- string) {
+//	for i := 0; ; i++ {
+//		c <- "ping"
+//	}
+//}
+//
+//func ponger(c chan<- string) {
+//	for i := 0; ; i++ {
+//		c <- "pong"
+//	}
+//}
+//
+//func printer(c <-chan string) {
+//	for {
+//		fmt.Println(<-c)
+//		time.Sleep(time.Second * 1)
+//	}
+//}
+
+func main() {
+	//for i := 0; i < 10; i++ {
+	go f(0)
+	//}
+
+	//var c chan string = make(chan string)
+	//	go pinger(c)
+	//	go ponger(c)
+	//	go printer(c)
+
+	c1 := make(chan string, 10)
+	c2 := make(chan string)
+
+	go func() {
+		for {
+			c1 <- "from1"
+			time.Sleep(time.Second * 2)
+		}
+	}()
+
+	go func() {
+		for {
+			c2 <- "from2"
+			time.Sleep(time.Second * 3)
+		}
+	}()
+
+	go func() {
+		for {
+			select {
+			case msg1 := <-c1:
+				fmt.Println(msg1)
+			case msg2 := <-c2:
+				fmt.Println(msg2)
+			case <-time.After(time.Second):
+				fmt.Println("timeout")
+				//default:
+				//	fmt.Println("nothing ready")
+			}
+		}
+	}()
+
+	var input string
+	fmt.Scanln(&input)
+}
