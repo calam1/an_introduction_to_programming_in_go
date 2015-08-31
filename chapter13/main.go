4560fdd78a3e53bd0af1861ef6f2a3660da63352
calam1 <chrislam67@gmail.com>
2015-08-31 13:07:55 -0500

    ch 13 sorting exercise

--- chapter13/main.go
+++ chapter13/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"container/list"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"sort"
 )
 
 func main() {
@@ -12,6 +14,46 @@ func main() {
 	//readFile("testclam.txt")
 	//readDirectory()
 	readDirectoryRecursively()
+
+	//container/list example
+	var llist list.List
+	llist.PushBack(1)
+	llist.PushBack(2)
+	llist.PushBack(3)
+
+	for e := llist.Front(); e != nil; e = e.Next() {
+		fmt.Println(e.Value.(int))
+	}
+
+	kids := []Person{
+		{"Chris", 9},
+		{"Sue", 9},
+		{"Brad", 9},
+		{"Jill", 9},
+		{"Jack", 10},
+	}
+	fmt.Println(kids)
+	sort.Sort(ByName(kids))
+	fmt.Println(kids)
+}
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+type ByName []Person
+
+func (this ByName) Len() int {
+	return len(this)
+}
+
+func (this ByName) Less(i, j int) bool {
+	return this[i].Name < this[j].Name
+}
+
+func (this ByName) Swap(i, j int) {
+	this[i], this[j] = this[j], this[i]
 }
 
 func createFile(txt string) {
